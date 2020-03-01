d120de5c8cf7128bf3c9593b301b5ff9d930bd7a
Yifan Bai <yfbai@uw.edu>
2020-02-29 19:46:09 -0800

    country data added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 // You can require libraries
 const d3 = require('d3')
 
@@ -7,24 +5,25 @@ const d3 = require('d3')
 // Prevalence of mental health disorders by disorder type
 const csvFile = require('../prevalence-by-mental-and-substance-use-disorder.csv');
 
-// You can include local JS files:
-const MyClass = require('./my-class');
-const myClassInstance = new MyClass();
-myClassInstance.sayHi();
-
 
 // You can load JSON files directly via require.
 // Note this does not add a network request, it adds
 // the data directly to your JavaScript bundle.
-const exampleData = require('./example-data.json');
+// const exampleData = require('./example-data.json');
 
 
 // Anything you put in the static folder will be available
 // over the network, e.g.
-d3.csv('carbon-emissions.csv')
-  .then((data) => {
-    console.log('Dynamically loaded CSV data', data);
-  })
+// Load the csv file
+var all_data = d3.csv(csvFile, function(data) {
+  return {
+      entity: data.Entity,
+      code: data.Code,
+      year: data.year,
+      schizophrenia: data["Schizophrenia (%)"]
+  };
+});
+
 
 var mymap = L.map('mapid', {
       minZoom: 1.4
@@ -54,7 +53,29 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     center: bounds.getCenter()
 }).addTo(mymap);
 
+L.geoJson(countriesData).addTo(map);
 
+function getColor(d) {
+  return d > 1000 ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 200  ? '#E31A1C' :
+         d > 100  ? '#FC4E2A' :
+         d > 50   ? '#FD8D3C' :
+         d > 20   ? '#FEB24C' :
+         d > 10   ? '#FED976' :
+                    '#FFEDA0';
+}
+
+function style(feature) {
+  return {
+      fillColor: getColor(feature.properties.density),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+  };
+}
 
  
   
